f5ef8340e9580dcc87da0e124f43261a80d8221f
Sreedharan Namboodiri C <sridhardigital22@gmail.com>
2023-06-15 09:41:49 +0000

    first commit

---
+++ src/components/Speedometer/index.js
@@ -0,0 +1,12 @@
+import {Component} from 'react'
+import './index.css'
+
+class Speedometer extends Component {
+  render() {
+    return (
+    <div className="bg-container">
+    </div>
+  )}
+}
+
+default export Speedometer
